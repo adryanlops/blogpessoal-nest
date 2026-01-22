fed34897b89b98eb79e9d12b1372d1b6bd1e7015
adryanlops <adylopesk8@gmail.com>
2026-01-22 11:37:17 -0300

    corrigindo variaveis no prod service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuarios/usuario.module';
--- src/data/services/prod.service.ts
+++ src/data/services/prod.service.ts
@@ -6,7 +6,11 @@ export class ProdService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      url: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       logging: false,
       dropSchema: false,
       ssl: {
